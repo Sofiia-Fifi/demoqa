4b35c93902fd1ad73df3e9d04124257478ae6cf2
Sofiia <sofiamyugrieva47@gmail.com>
2025-03-28 11:36:27 +0300

    Лекция 8

--- tests/test_hw/test_check_text.py
+++ tests/test_hw/test_check_text.py
@@ -5,14 +5,20 @@ from pages.elements_page import ElementsPage
 def test_check_text_1(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    assert demo_qa_page.equal_url()
     assert demo_qa_page.footer_text.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
 def test_check_text_2(browser):
     demo_qa_page = DemoQa(browser)
-    demo_qa_page.visit()
     elements_page = ElementsPage(browser)
-    assert demo_qa_page.equal_url()
+    demo_qa_page.visit()
     demo_qa_page.button_elements.click()
-    assert elements_page.equal_url()
     assert elements_page.centre_text.get_text() == 'Please select an item from left to start practice.'
+
+def test_page_elements(browser):
+    elements_page = ElementsPage(browser)
+    elements_page.visit()
+
+    assert elements_page.icon.exist()
+    assert elements_page.btn_sidebar_first.exist()
+    assert elements_page.btn_sidebar_first_textbox.exist()
+    # assert elements_page.text_elements.get_text() == 'Please select an item from left to start practice.'
