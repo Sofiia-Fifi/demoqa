32c902a169ab87e5dd858c88695c89b2f4b5ad65
Sofiia <sofiamyugrieva47@gmail.com>
2025-04-12 11:48:20 +0300

    ДЗ 12

--- tests/test_hw/test_sort.py
+++ tests/test_hw/test_sort.py
@@ -7,6 +7,8 @@ import time
 def test_sort(browser):
     webtable_page = WebTables(browser)
     webtable_page.visit()
-    webtable_page.columnheader.click()
-    time.sleep(2)
-    assert webtable_page.columnheader.get_dom_attribute('class') == 'rt-th rt-resizable-header -sort-asc -cursor-pointer'
+
+    for header in webtable_page.columnheader.find_elements():
+        header.click()
+        time.sleep(2)
+        assert header.get_dom_attribute('class') == 'rt-th rt-resizable-header -sort-asc -cursor-pointer'
